f076516658c7cdf52ccb4d4858b8eb3fe92f2e20
myinusa <myinusa@hotmail.co.uk>
2025-02-07 19:13:48 +0000

    fix: update ESLint configurations to use FlatCompat and new plugins refactor: modernize package.json dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,45 @@
-import eslint from "@eslint/js";
+import eslintjs from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
 import tseslint from "typescript-eslint";
 import sonarjs from "eslint-plugin-sonarjs";
 import unicorn from "eslint-plugin-unicorn";
 import eslintConfigPrettier from "eslint-config-prettier";
-// import eslint_js from "@eslint/js";
 import node from "eslint-plugin-n";
 import promise from "eslint-plugin-promise";
 import securityNode from "eslint-plugin-security-node";
-import { fixupPluginRules } from "@eslint/compat";
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 
 const importRules = {
   "n/no-unpublished-import": "off",
   "n/no-missing-import": "off",
 };
 
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  resolvePluginsRelativeTo: __dirname,
+  recommendedConfig: eslintjs.configs.recommended,
+  allConfig: eslintjs.configs.all,
+});
+
+
 /** @type {Awaited<import('typescript-eslint').Config>} */
 export default tseslint.config(
-  eslint.configs.recommended,
+  eslintjs.configs.all,
   ...tseslint.configs.recommended,
   node.configs["flat/recommended"],
   unicorn.configs["flat/all"],
-  {
-    files: ["**/*.ts"],
-  },
+  ...fixupConfigRules(
+    compat.extends(
+      "plugin:promise/recommended",
+      // "plugin:import/recommended",
+    ),
+  ),
+  { files: ["src/**/*.{ts}"] },
   {
     ignores: ["node_modules", "build", "dist", "logs"],
   },
@@ -54,8 +71,8 @@ export default tseslint.config(
   },
   {
     rules: {
+      ...securityNode.configs.recommended.rules,
       ...sonarjs.configs.recommended.rules,
-      //   ...unicorn.configs.recommended.rules,
       ...eslintConfigPrettier.rules,
       ...importRules,
       "unicorn/prefer-top-level-await": 0,
