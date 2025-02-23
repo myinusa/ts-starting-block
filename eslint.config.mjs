b848008a5935cc3178a99bd54a8dd4ffc93896e4
myinusa <myinusa@hotmail.co.uk>
2025-02-23 19:36:01 +0000

    feat: update code-workspace and ESLint configuration for TypeScript compatibility fix: add `no-magic-numbers` rule to ESLint configuration fix: remove `sonarjs/unused-import` rule from ESLint configuration feat: add `simple-import-sort` plugin to ESLint configuration fix: add `eslint-plugin-simple-import-sort` package to `package.json` refactor: update `typescript-eslint/parser`, `eslint-config-prettier`, and other dependencies in `package.json` refactor: update `node.js` related rules in `package.json` style: remove `sonarjs/no-dead-store` rule from ESLint configuration perf: improve performance of ESLint configuration by using recommended configurations instead of manually specifying them

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,62 @@
-import eslintjs from "@eslint/js";
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
 import { FlatCompat } from "@eslint/eslintrc";
-import tseslint from "typescript-eslint";
-import sonarjs from "eslint-plugin-sonarjs";
-import unicorn from "eslint-plugin-unicorn";
+import eslintjs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
+import _import from "eslint-plugin-import";
 import node from "eslint-plugin-n";
 import promise from "eslint-plugin-promise";
 import securityNode from "eslint-plugin-security-node";
-import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import eslintPluginSimpleImportSort from "eslint-plugin-simple-import-sort";
+import sonarjs from "eslint-plugin-sonarjs";
+import unicorn from "eslint-plugin-unicorn";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import tseslint from "typescript-eslint";
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
+const importGroups = [
+  // Side effect imports.
+  [String.raw`^\u0000`],
+  // Packages.
+  // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
+  [String.raw`^@?\w`],
+  // Absolute imports and other imports such as Vue-style `@/foo`.
+  // Anything not matched in another group.
+  ["^"],
+  // Relative imports.
+  // Anything that starts with a dot
+  [String.raw`^\.`],
+  // Typings
+  [String.raw`^@?\w.*\u0000$`, String.raw`^[^.].*\u0000$`, String.raw`^\..*\u0000$`],
+];
+
+
 const importRules = {
   "n/no-unpublished-import": "off",
   "n/no-missing-import": "off",
+  "n/no-process-exit": "off",
+  "import/first": "error",
+  "import/newline-after-import": "error",
+  "import/no-duplicates": "error",
+  "simple-import-sort/imports": [
+    1,
+    {
+      groups: importGroups,
+    },
+  ],
+  "no-restricted-imports": [
+    "error",
+    {
+      patterns: [
+        {
+          group: ["../*"],
+          message: "For imports of parent elements use better path aliases. For example, @domain/shared.",
+        },
+      ],
+    },
+  ],
 };
 
 const compat = new FlatCompat({
@@ -29,14 +69,19 @@ const compat = new FlatCompat({
 
 /** @type {Awaited<import('typescript-eslint').Config>} */
 export default tseslint.config(
-  eslintjs.configs.all,
+  // eslintjs.configs.all,
+  eslintjs.configs.recommended,
   ...tseslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
   node.configs["flat/recommended"],
   unicorn.configs["flat/all"],
   ...fixupConfigRules(
     compat.extends(
       "plugin:promise/recommended",
       // "plugin:import/recommended",
+      "plugin:import/recommended",
+      "plugin:import/typescript",
     ),
   ),
   { files: ["src/**/*.{ts}"] },
@@ -48,6 +93,10 @@ export default tseslint.config(
       sonarjs: sonarjs,
       "security-node": fixupPluginRules(securityNode),
       promise: fixupPluginRules(promise),
+      "simple-import-sort": eslintPluginSimpleImportSort,
+      // https://github.com/import-js/eslint-plugin-import?tab=readme-ov-file
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      import: fixupPluginRules(_import),
     },
     settings: {
       n: {
@@ -75,13 +124,16 @@ export default tseslint.config(
       ...sonarjs.configs.recommended.rules,
       ...eslintConfigPrettier.rules,
       ...importRules,
+      "no-magic-numbers": ["warn", { ignoreArrayIndexes: true }],
       "unicorn/prefer-top-level-await": 0,
-      "sonarjs/unused-import": 0,
+      "sonarjs/unused-import": 1,
       "@typescript-eslint/no-unused-vars": 0,
       "unicorn/prevent-abbreviations": 0,
       "unicorn/prefer-spread": 1,
+      "unicorn/no-null": 1,
       "sonarjs/no-dead-store": 0,
       "sonarjs/no-unused-vars": 0,
+      "sonarjs/pseudo-random": 0,
     },
   },
 );
